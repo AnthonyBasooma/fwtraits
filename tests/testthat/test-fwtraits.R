fbe33177bff8b818d76a4d4af5a73fd8193916c4
AnthonyBasooma <bas4ster@gmail.com>
2024-12-07 14:00:19 +0100

    commit package website

--- tests/testthat/test-fwtraits.R
+++ tests/testthat/test-fwtraits.R
@@ -1,7 +1,30 @@
 #
 # #encrypted token for my api key
 
+searchdata <- fw_searchdata(organismgroup = 'fi', ecoparams = 'migration', cachefolder = 'cache')
 
+splitdata <- fw_split(data = "Abramis brama", organismgroup = 'fi',
+                      ecoparams = 'migration', cachefolder = 'cache')
 
+fetchdata <- fw_fetchdata(data = "Abramis brama", organismgroup = 'fi',
+                      ecoparams = 'migration', cachefolder = 'cache',
+                      sanitize = TRUE)
 
+test_that(desc = 'List is generated species with migration types',
+          code = {expect_type(searchdata, 'list')
+            })
+
+test_that(desc = 'Dataframe with a particular species ',
+          code = {
+            expect_s3_class (splitdata, 'data.frame')
+
+            expect_equal(nrow(splitdata), 1)
+            })
+
+test_that(desc = 'Dataframe with sanitized species data ',
+          code = {
+            expect_s3_class (fetchdata, 'data.frame')
+
+            expect_equal(nrow(fetchdata), 1)
+          })
 
